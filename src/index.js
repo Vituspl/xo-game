68fbf255de7de64b764ea0ac61c52c76eae9a59d
VitusPl <victorplug@gmail.com>
2023-11-08 17:47:13 +0800

    простая игра готова

--- src/index.js
+++ src/index.js
@@ -42,7 +42,15 @@ class Board extends React.Component {
          *  xIsNext: !this.state.xIsNext,
          *  После этих изменений «X» и «O» будут чередоваться.
          */
+        /**  можем изменить метод handleClick класса Board для выхода из функции и
+         *  игнорировании клика, если кто-то уже победил или если клетка уже
+         *  заполнена, проверкой условия :
+         *  if (calculateWinner(squares) || squares[i]) { return; }
+         */
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -68,10 +76,16 @@ class Board extends React.Component {
     }
 
     render() {
-        /** Также изменим текст «status» в методе render класса Board так,
-         * чтобы он отображал какой игрок ходит следующим:
+        /** вызывать calculateWinner(squares) внутри метода render класса Board,
+         * чтобы проверять, выиграл ли игрок.
          */
-        const status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Выиграл ' + winner;
+        } else {
+            status = 'Следующий ход: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -116,3 +130,28 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game/>);
+
+/** Функция calculateWinner(squares) получает массив из 9 клеток, проверяет
+ *  победителя и возвращает 'X', 'O' или null.
+ *  Будем вызывать calculateWinner(squares) внутри метода render класса Board,
+ *  чтобы проверять, выиграл ли игрок.
+ */
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
